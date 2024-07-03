3907eb36775deac8c6573f002cd19e480700765e
helloashik17 <asiknoman150@gmail.com>
2024-07-04 00:35:55 +0600

    authetication and blog services added

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+    origin: 'https://fantastic-malabi-f7d0be.netlify.app',
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 
 // MongoDB Connection URL
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const verifyJWT = (req, res, next) => {
+    const token = req.headers.authorization;
+    
+    if (!token) {
+        return res.status(401).send('Access Denied. No token provided.');
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).send('Access Denied. Invalid Token.');
+    }
+};
 
 async function run() {
     try {
@@ -21,9 +45,68 @@ async function run() {
         console.log("Connected to MongoDB");
 
         const db = client.db('my-portfolio');
+        const userCollection = db.collection('users');
         const projectCollection = db.collection('projects');
         const skillCollection = db.collection('skills');
         const blogCollection = db.collection('blogs');
+
+        // jwt checking for testing
+        app.get('/protected', verifyJWT, (req, res) => {
+            res.send('Welcome to the protected route');
+        });
+
+        // User Login
+        app.post('/login', async (req, res) => {
+            const { email, password } = req.body;
+
+            // Find user by email
+            const user = await userCollection.findOne({ email, password });
+            if (!user) {
+                return res.status(401).json({ message: 'Invalid email or password' });
+            }
+
+            // Generate JWT token
+            const jwtPayload = { email: user.email, role: user.role };
+            const accessToken = jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '1h' });
+            const refreshToken = jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+            res
+            .cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'none',
+                maxAge: 1000 * 60 * 60 * 24 * 365,
+            })
+            .json({
+                success: true,
+                message: 'Login successful',
+                accessToken
+            });
+        });
+
+        // Generate new access token using refresh token
+        app.post('/refresh-token', (req, res) => {
+            const refreshToken = req.cookies.refreshToken;
+            if (!refreshToken) {
+                return res.status(401).send('Access Denied. No refresh token provided.');
+            }
+        
+            try {
+                const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET);
+                console.log(decoded);
+                const jwtPayload = { email: decoded.email, role: decoded.role };
+                const accessToken = jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '1h' });
+                console.log(accessToken);
+    
+                res.json({
+                    success: true,
+                    message: 'Access token is retrieved successfully.',
+                    accessToken
+                });
+            } catch (error) {
+                return res.status(400).send('Invalid refresh token.');
+            }
+        });
 
         // Get All Projects
         app.get('/projects', async (req, res) => {
@@ -38,7 +121,7 @@ async function run() {
         });
 
         // Create A New Project
-        app.post('/projects', async (req, res) => {
+        app.post('/projects', verifyJWT, async (req, res) => {
             const project = req.body;
             const result = await projectCollection.insertOne(project);
             res.send(result);
@@ -57,7 +140,7 @@ async function run() {
         });
 
         // Create A New Skill
-        app.post('/skills', async (req, res) => {
+        app.post('/skills', verifyJWT, async (req, res) => {
             const skill = req.body;
             const result = await skillCollection.insertOne(skill);
             res.send(result);
@@ -77,7 +160,7 @@ async function run() {
         });
 
         // Create A New Blog
-        app.post('/blogs', async (req, res) => {
+        app.post('/blogs', verifyJWT, async (req, res) => {
             const blog = req.body;
             const result = await blogCollection.insertOne(blog);
             res.send(result);
